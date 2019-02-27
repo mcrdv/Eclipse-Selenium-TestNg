8cc43744a9fd823cdd549eee67190b91dcff4e79
mcrdv <miguel.crdv@gmail.com>
2019-02-27 13:09:12 -0600

    Se cera pruebas selenium con eclipse y testng

---
+++ src/AmazonPrueba/AmazonPrueba.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package AmazonPrueba;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+/**
+ * @author Tasisoft
+ *
+ */
+public class AmazonPrueba {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	      WebDriver driver;
+	        driver = new ChromeDriver();
+	        driver.manage().window().maximize();
+	        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	       
+	     // busqueda en el navegador
+	        driver.get("https://www.amazon.com.mx");
+
+	        //seleccionar el dropdown
+	        WebElement Searchdropdown = driver.findElement(By.id("searchDropdownBox"));
+	        Searchdropdown.click();
+
+	        //seleccionar videojuegos en el dropdown
+	        Select videojuegos = new Select(Searchdropdown);
+	        //videojuegos.deselectAll();
+	        videojuegos.selectByVisibleText("Videojuegos");
+	        //videojuegos1.click();
+
+	        //se busca el textbox de busqueda///
+	          WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+
+	        //agregar la busqueda en el textbox//
+	        searchBox.sendKeys("resident evil2");
+
+	         //Clic en el boton buscar
+	         WebElement searchButton = driver.findElement(By.id("//*[@id=nav-search]/form/div[2]/div/input"));
+	         searchButton.click();
+
+
+	         WebElement residentevil2 = driver.findElement(By.name(""));
+	        
+
+	}
+
+}
